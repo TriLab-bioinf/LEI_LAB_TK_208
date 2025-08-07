fb73f3dd978d38f22c3c57578abfda67872ec03c
Hernan Lorenzi <Hernan.Lorenzi@nih.gov>
2025-08-07 17:01:21 -0400

    log10 transformation of contacts and bed saving for all resol

--- R/scripts/plots_virtual4C.R
+++ R/scripts/plots_virtual4C.R
@@ -3,6 +3,63 @@ plot_virtual_4C <- function(s, my_hiccup_files, resolution){
 my_key <- paste0(s,":",resolution)
 bedpe <- tibble()
 
+# Import HiC data for region of interest
+hicFile <- list.files(path = paste(WD,"juicer_results",s,sep = "/"),
+                      pattern = paste0("*inter_30.hic"), 
+                      full.names = TRUE, , recursive = TRUE)
+
+print(paste("Importing Hi-C data from file:", hicFile))
+hicDataChromRegion <- readHic(file = hicFile,
+                              chrom = "3R", assembly = Dm6,
+                              resolution = resolution, res_scale = "BP", norm = "KR"
+)
+
+# Add contact scores to my_regions_of_interest.gr for plotting ------------------
+
+# Entering observed twice to account for from and to contact coords that are in two diff rows
+
+contacts.df <- hicDataChromRegion |> rename("3R_A"="x1", "3R_B"="y1") |> mutate(x2=x1+resolution-2, y2=y1+resolution-2) |> filter( ( ( (x1-14308801) * ( x2-14304362) ) < 0 )| (( (y1-14308801) * (y2-14304362)) < 0))
+
+# Define a GRanges object for a specific region
+my_roi_all.gr <- GRanges(seqnames = "chr3R",
+                         ranges = IRanges(start = c(contacts.df$x1, contacts.df$y1), 
+                                          end = c(contacts.df$x2, contacts.df$y2)))
+
+mcols(my_roi_all.gr)$counts <- rep(contacts.df$counts, n=2)
+
+# Merge overlaping ranges
+my_roi_all_merged.gr <- reduce(my_roi_all.gr)
+
+# Find overlaps between my_regions_of_interest.gr and my_regions_of_interest_merged.gr
+overlaps <- findOverlaps(my_regions_of_interest.gr, my_roi_all_merged.gr, type = "any", ignore.strand = TRUE)
+
+# Aggregate metadata for the merged ranges (example: concatenate gene names)
+# Create an empty list to store aggregated metadata
+aggregated_metadata <- list()
+for (i in seq_along(my_roi_all_merged.gr)) {
+  # Get original ranges that overlap with the current reduced range
+  original_indices <- queryHits(overlaps)[subjectHits(overlaps) == i]
+  
+  # Concatenate gene names
+  aggregated_metadata[[i]] <- sum(my_roi_all.gr$counts[original_indices])
+}
+
+# Add aggregated metadata to the reduced GRanges object
+mcols(my_roi_all_merged.gr)$counts_sum <- unlist(aggregated_metadata)
+
+# delete my_roi_all_merged.gr intervals overlapping coords 14304362-14308801
+my_roi_all_merged.gr <- my_roi_all_merged.gr[(end(my_roi_all_merged.gr) < 14304362 | start(my_roi_all_merged.gr) > 14308801)]
+
+# Convert the GRanges object to a data frame for plotting
+my_roi_all_merged.df <- as.data.frame(my_roi_all_merged.gr)[,1:3] |> 
+  mutate(score = if_else(my_roi_all_merged.gr$counts_sum == 0, 0, log10(mcols(my_roi_all_merged.gr)$counts_sum) ), strand = '.') |> 
+  filter( ( (start-14308801) * ( end-14304362) ) > 0 )
+
+# Save Virtual4C HiC data as bed file for plotting with other programs
+write_csv(x = my_roi_all_merged.df |> mutate(score = 10**score ), file = paste0(WD,"/R/render/virtual-4C-analysis_files/Tables/hic_contacts-",sample,"-",resolution,".bed"), 
+          col_names = FALSE)
+
+
 # Subset hiccup results to region of interest containing bait start-end
 tryCatch({
   my_hiccup_files[[my_key]]
@@ -69,47 +126,6 @@ my_interacting_genes <- data.frame(gene = overlapping_gene_symbols$SYMBOL, color
 my_interacting_genes <- my_interacting_genes[order(my_interacting_genes$gene != "su(Hw)"),]
 
 
-# # Add contact scores to my_regions_of_interest.gr for plotting ------------------
-# # Entering observed twice to account for from and to contact coords that are in two diff rows
-# bedpe_all_bins <- my_hiccup_files[[my_key]] |> 
-#   filter(  ((((x1-14308801) * (x2-14304362)) < 0 & chr1 == "chr3R")| 
-#               (((y1-14308801) * (y2-14304362)) < 0 & chr2 == "chr3R")))
-# 
-#  # Define a GRanges object for a specific region
-#  my_roi_all.gr <- GRanges(seqnames = "chr3R",
-#                           ranges = IRanges(start = c(bedpe_all_bins$x1, bedpe_all_bins$y1), 
-#                                                        end = c(bedpe_all_bins$x2, bedpe_all_bins$y2)))
-#            
-# mcols(my_roi_all.gr)$observed <- rep(bedpe_all_bins$observed, n=2)
-# 
-# # Merge overlaping ranges
-# my_roi_all_merged.gr <- reduce(my_roi_all.gr)
-# 
-# # Find overlaps between my_regions_of_interest.gr and my_regions_of_interest_merged.gr
-# 
-# # Aggregate metadata for the merged ranges (example: concatenate gene names)
-# # Create an empty list to store aggregated metadata
-# aggregated_metadata <- list()
-# for (i in seq_along(my_roi_all_merged.gr)) {
-#   # Get original ranges that overlap with the current reduced range
-#   original_indices <- queryHits(overlaps)[subjectHits(overlaps) == i]
-#   
-#   # Concatenate gene names
-#   aggregated_metadata[[i]] <- sum(my_roi_all.gr$observed[original_indices])
-# }
-# 
-# # Add aggregated metadata to the reduced GRanges object
-# mcols(my_roi_all_merged.gr)$observed_sum <- unlist(aggregated_metadata)
-# 
-# # delete my_roi_all_merged.gr intervals overlapping coords 14304362-14308801
-# my_roi_all_merged.gr <- my_roi_all_merged.gr[(end(my_roi_all_merged.gr) < 14304362 | start(my_roi_all_merged.gr) > 14308801)]
-# 
-# # Convert the GRanges object to a data frame for plotting
-# my_roi_all_merged.df <- as.data.frame(my_roi_all_merged.gr)[,1:3] |> 
-#   mutate(score = mcols(my_roi_all_merged.gr)$observed_sum, strand = '.')
-
-
-
 # Search regions of interest (Contacts) that overlap with SuHw_ChIRP_conc.gr ------------------
 SuHw_ChIRP_overlap <- findOverlaps(SuHw_ChIRP_conc.gr, my_regions_of_interest.gr, type = "any", ignore.strand = TRUE, maxgap = 0)
 SuHw_ChIRP_conc.overlap.gr <- SuHw_ChIRP_conc.gr[from(SuHw_ChIRP_overlap) |> unique(),]
@@ -122,61 +138,13 @@ bedpe <- bedpe |> mutate(overlap = if_else((x1 %in% my_regions_of_interest_overl
                                             (y1 %in% my_regions_of_interest_overlap.start & 
                                                ((y1-(14308589+resolution)) * (y2-(14304455-resolution))) > 0) , TRUE, FALSE))
 
-# Import HiC data for region of interest
-hicFile <- list.files(path = paste(WD,"juicer_results",s,sep = "/"),
-                      pattern = paste0("*inter_30.hic"), 
-                      full.names = TRUE, , recursive = TRUE)
 
-print(paste("Importing Hi-C data from file:", hicFile))
+# Subet hicDataChromRegion and my_roi_all_merged.df to region of interest
+hicDataChromRegion <- hicDataChromRegion[hicDataChromRegion$`3R_A` >= region_start & hicDataChromRegion$`3R_A` <= region_end & 
+                                           hicDataChromRegion$`3R_B` >= region_start & hicDataChromRegion$`3R_B` <= region_end,]
 
-hicDataChromRegion <- readHic(file = hicFile,
-                              chrom = "3R", assembly = Dm6,
-                              chromstart = region_start, chromend = region_end,
-                              resolution = resolution, res_scale = "BP", norm = "KR"
-)
-
-# Add contact scores to my_regions_of_interest.gr for plotting ------------------
-
-# Entering observed twice to account for from and to contact coords that are in two diff rows
-
-contacts.df <- hicDataChromRegion |> rename("3R_A"="x1", "3R_B"="y1") |> mutate(x2=x1+resolution-2, y2=y1+resolution-2) |> filter( ( ( (x1-14308801) * ( x2-14304362) ) < 0 )| (( (y1-14308801) * (y2-14304362)) < 0))
-
-# Define a GRanges object for a specific region
-my_roi_all.gr <- GRanges(seqnames = "chr3R",
-                         ranges = IRanges(start = c(contacts.df$x1, contacts.df$y1), 
-                                          end = c(contacts.df$x2, contacts.df$y2)))
-
-mcols(my_roi_all.gr)$counts <- rep(contacts.df$counts, n=2)
-
-# Merge overlaping ranges
-my_roi_all_merged.gr <- reduce(my_roi_all.gr)
-
-# Find overlaps between my_regions_of_interest.gr and my_regions_of_interest_merged.gr
-overlaps <- findOverlaps(my_regions_of_interest.gr, my_roi_all_merged.gr, type = "any", ignore.strand = TRUE)
-
-# Aggregate metadata for the merged ranges (example: concatenate gene names)
-# Create an empty list to store aggregated metadata
-aggregated_metadata <- list()
-for (i in seq_along(my_roi_all_merged.gr)) {
-  # Get original ranges that overlap with the current reduced range
-  original_indices <- queryHits(overlaps)[subjectHits(overlaps) == i]
-  
-  # Concatenate gene names
-  aggregated_metadata[[i]] <- sum(my_roi_all.gr$counts[original_indices])
-}
-
-# Add aggregated metadata to the reduced GRanges object
-mcols(my_roi_all_merged.gr)$counts_sum <- unlist(aggregated_metadata)
-
-# delete my_roi_all_merged.gr intervals overlapping coords 14304362-14308801
-my_roi_all_merged.gr <- my_roi_all_merged.gr[(end(my_roi_all_merged.gr) < 14304362 | start(my_roi_all_merged.gr) > 14308801)]
-
-# Convert the GRanges object to a data frame for plotting
-my_roi_all_merged.df <- as.data.frame(my_roi_all_merged.gr)[,1:3] |> 
-  mutate(score = if_else(my_roi_all_merged.gr$counts_sum == 0, 0, log10(mcols(my_roi_all_merged.gr)$counts_sum) ), strand = '.') |> 
-  filter( ( (start-14308801) * ( end-14304362) ) > 0 )
-
-
+my_roi_all_merged.df <- my_roi_all_merged.df[my_roi_all_merged.df$start >= region_start & 
+                                              my_roi_all_merged.df$end <= region_end,]
 
 ################################
 # Make virtual-4C plot
